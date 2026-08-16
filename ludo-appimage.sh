f1b6e06ca13ad69dd2b8f1e64a17c07a9b031e5f
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-08-16 10:14:57 -0300

    Update library dependencies in ludo-appimage.sh
    Added additional library dependencies for Ludo application execution.

--- ludo-appimage.sh
+++ ludo-appimage.sh
@@ -48,6 +48,17 @@ wget -c "$URL_BIN2"
 mv sharun-x86_64 sharun && chmod +x sharun && mv sharun ./Ludo-Linux-x11-x86_64* && cd Ludo-Linux-x11-x86_64* && xvfb-run -a ./sharun l -p -v -e -s -k ./ludo \
   	/usr/lib/x86_64-linux-gnu/dri/* \
 	/usr/lib/x86_64-linux-gnu/gconv/* \
+	/lib/x86_64-linux-gnu/libGL.so* \
+	/lib/x86_64-linux-gnu/libm.so* \
+	/lib/x86_64-linux-gnu/libc.so* \
+	/lib/x86_64-linux-gnu/libGLdispatch.so* \
+	/lib/x86_64-linux-gnu/libGLX.so* \
+	/lib/x86_64-linux-gnu/libX11.so* \
+	/lib/x86_64-linux-gnu/libxcb.so* \
+	/lib/x86_64-linux-gnu/libXau.so* \
+	/lib/x86_64-linux-gnu/libXdmcp.so* \
+	/lib/x86_64-linux-gnu/libbsd.so* \
+	/lib/x86_64-linux-gnu/libmd.so* \
 	/usr/lib/x86_64-linux-gnu/libopenal.so* \
 	/usr/lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 \
 	/usr/lib/x86_64-linux-gnu/libm.so.6 \
